81fd024c68c8bbf5df06b369e357b7621969fb34
RVVZ <145824934+revvze@users.noreply.github.com>
2024-08-18 17:55:20 -0600

    oompa loompa doopity doo

--- plugins/scpcb_animations/sh_plugin.lua
+++ plugins/scpcb_animations/sh_plugin.lua
@@ -15,277 +15,61 @@ function PLUGIN:PlayerShouldTaunt(ply, act)
     ply:ForceSequence(sequence)
 end
 
--- Class-D
-ix.anim.classd = {
+-- Foundation Passive
+ix.anim.foundation = {
     normal = {
         [ACT_MP_STAND_IDLE] = {"idle", "idle"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
+        [ACT_MP_CROUCH_IDLE] = {"crouch", "crouch"},
         [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
+        [ACT_MP_CROUCHWALK] = {"crouch_walk", "crouch_walk"},
         [ACT_MP_RUN] = {"run", "run"},
         [ACT_LAND] = {ACT_RESET, ACT_RESET}
     },
     pistol = {
         [ACT_MP_STAND_IDLE] = {"idle", "idle"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
+        [ACT_MP_CROUCH_IDLE] = {"crouch", "crouch"},
         [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
+        [ACT_MP_CROUCHWALK] = {"crouch_walk", "crouch_walk"},
         [ACT_MP_RUN] = {"run", "run"},
         [ACT_LAND] = {ACT_RESET, ACT_RESET}
     },
     smg = {
         [ACT_MP_STAND_IDLE] = {"idle", "idle"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
+        [ACT_MP_CROUCH_IDLE] = {"crouch", "crouch"},
         [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
+        [ACT_MP_CROUCHWALK] = {"crouch_walk", "crouch_walk"},
         [ACT_MP_RUN] = {"run", "run"},
         [ACT_LAND] = {ACT_RESET, ACT_RESET}
     },
     shotgun = {
         [ACT_MP_STAND_IDLE] = {"idle", "idle"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
+        [ACT_MP_CROUCH_IDLE] = {"crouch", "crouch"},
         [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
+        [ACT_MP_CROUCHWALK] = {"crouch_walk", "crouch_walk"},
         [ACT_MP_RUN] = {"run", "run"},
         [ACT_LAND] = {ACT_RESET, ACT_RESET}
     },
     grenade = {
         [ACT_MP_STAND_IDLE] = {"idle", "idle"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
+        [ACT_MP_CROUCH_IDLE] = {"crouch", "crouch"},
         [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
+        [ACT_MP_CROUCHWALK] = {"crouch_walk", "crouch_walk"},
         [ACT_MP_RUN] = {"run", "run"},
         [ACT_LAND] = {ACT_RESET, ACT_RESET}
     },
     melee = {
         [ACT_MP_STAND_IDLE] = {"idle", "idle"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
+        [ACT_MP_CROUCH_IDLE] = {"crouch", "crouch"},
         [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
+        [ACT_MP_CROUCHWALK] = {"crouch_walk", "crouch_walk"},
         [ACT_MP_RUN] = {"run", "run"},
         [ACT_LAND] = {ACT_RESET, ACT_RESET}
     },
     glide = ACT_IDLE,
 }
 
--- Maintenance
-ix.anim.maintenance = {
-    normal = {
-        [ACT_MP_STAND_IDLE] = {"idle", "idle"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET}
-    },
-    pistol = {
-        [ACT_MP_STAND_IDLE] = {"idle", "idle"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET}
-    },
-    smg = {
-        [ACT_MP_STAND_IDLE] = {"idle", "idle"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET}
-    },
-    shotgun = {
-        [ACT_MP_STAND_IDLE] = {"idle", "idle"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET}
-    },
-    grenade = {
-        [ACT_MP_STAND_IDLE] = {"idle", "idle"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET}
-    },
-    melee = {
-        [ACT_MP_STAND_IDLE] = {"idle", "idle"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET}
-    },
-    glide = ACT_IDLE,
-}
-
--- Researcher
-ix.anim.researcher = {
-    normal = {
-        [ACT_MP_STAND_IDLE] = {"idle", "idle"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET}
-    },
-    pistol = {
-        [ACT_MP_STAND_IDLE] = {"idle", "idle"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET}
-    },
-    smg = {
-        [ACT_MP_STAND_IDLE] = {"idle", "idle"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET}
-    },
-    shotgun = {
-        [ACT_MP_STAND_IDLE] = {"idle", "idle"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET}
-    },
-    grenade = {
-        [ACT_MP_STAND_IDLE] = {"idle", "idle"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET}
-    },
-    melee = {
-        [ACT_MP_STAND_IDLE] = {"idle", "idle"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET}
-    },
-    glide = ACT_IDLE,
-}
-
--- Guard
-ix.anim.guard = {
-    normal = {
-        [ACT_MP_STAND_IDLE] = {"idle", "idle_look"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET}
-    },
-    pistol = {
-        [ACT_MP_STAND_IDLE] = {"idle", "idle"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET}
-    },
-    smg = {
-        [ACT_MP_STAND_IDLE] = {"idle", "idle"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET}
-    },
-    shotgun = {
-        [ACT_MP_STAND_IDLE] = {"idle", "idle"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET}
-    },
-    grenade = {
-        [ACT_MP_STAND_IDLE] = {"idle", "idle"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET},
-        attack = "throw_grenade"
-    },
-    melee = {
-        [ACT_MP_STAND_IDLE] = {"idle", "idle"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET},
-        attack = "throw_grenade"
-    },
-    glide = ACT_IDLE,
-}
-
--- NTF
-ix.anim.ntf = {
-    normal = {
-        [ACT_MP_STAND_IDLE] = {"idle", "idle_look"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET}
-    },
-    pistol = {
-        [ACT_MP_STAND_IDLE] = {"idle", "idle"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET}
-    },
-    smg = {
-        [ACT_MP_STAND_IDLE] = {"idle", "idle"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET}
-    },
-    shotgun = {
-        [ACT_MP_STAND_IDLE] = {"idle", "idle"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET}
-    },
-    grenade = {
-        [ACT_MP_STAND_IDLE] = {"idle", "idle"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET},
-        attack = "throw_grenade"
-    },
-    melee = {
-        [ACT_MP_STAND_IDLE] = {"idle", "idle"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET},
-        attack = "throw_grenade"
-    },
-    glide = ACT_IDLE,
-}
-
--- Chaos
-ix.anim.chaos = {
+-- Combatant
+ix.anim.combatant = {
     normal = {
         [ACT_MP_STAND_IDLE] = {"idle", "idle_look"},
         [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
@@ -498,6 +282,7 @@ ix.anim.scp096 = {
     glide = ACT_IDLE,
 }
 
+-- SCP 106
 ix.anim.scp106 = {
     normal = {
         [ACT_MP_STAND_IDLE] = {"idle", "idle"},
@@ -547,60 +332,6 @@ ix.anim.scp106 = {
         [ACT_MP_RUN] = {"walk", "walk"},
         [ACT_LAND] = {ACT_RESET, ACT_RESET},
         attack = "attack"
-    },
-    glide = ACT_IDLE,
-}
-
--- SCP-427-1
-ix.anim.scp427 = {
-    normal = {
-        [ACT_MP_STAND_IDLE] = {"standing_idle", "standing_idle"},
-        [ACT_MP_CROUCH_IDLE] = {"crouch_idle", "crouch_idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"crouchwalk", "crouchwalk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET}
-    },
-    pistol = {
-        [ACT_MP_STAND_IDLE] = {"standing_idle", "standing_idle"},
-        [ACT_MP_CROUCH_IDLE] = {"crouch_idle", "crouch_idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"crouchwalk", "crouchwalk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET}
-    },
-    smg = {
-        [ACT_MP_STAND_IDLE] = {"standing_idle", "standing_idle"},
-        [ACT_MP_CROUCH_IDLE] = {"crouch_idle", "crouch_idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"crouchwalk", "crouchwalk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET}
-    },
-    shotgun = {
-        [ACT_MP_STAND_IDLE] = {"standing_idle", "standing_idle"},
-        [ACT_MP_CROUCH_IDLE] = {"crouch_idle", "crouch_idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"crouchwalk", "crouchwalk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET}
-    },
-    grenade = {
-        [ACT_MP_STAND_IDLE] = {"standing_idle", "standing_idle"},
-        [ACT_MP_CROUCH_IDLE] = {"crouch_idle", "crouch_idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"crouchwalk", "crouchwalk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET}
-    },
-    melee = {
-        [ACT_MP_STAND_IDLE] = {"standing_idle", "standing_idle"},
-        [ACT_MP_CROUCH_IDLE] = {"crouch_idle", "crouch_idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"crouchwalk", "crouchwalk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET},
-        attack = "spitter_melee_01"
     },
     glide = ACT_IDLE,
 }
@@ -873,65 +604,3 @@ ix.anim.scp1048a = {
     },
     glide = ACT_IDLE,
 }
-
--- SCP-1499
-ix.anim.scp1499 = {
-    normal = {
-        [ACT_MP_STAND_IDLE] = {"idle", "idle"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET}
-    },
-    pistol = {
-        [ACT_MP_STAND_IDLE] = {"idle", "idle"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET}
-    },
-    smg = {
-        [ACT_MP_STAND_IDLE] = {"idle", "idle"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET}
-    },
-    shotgun = {
-        [ACT_MP_STAND_IDLE] = {"idle", "idle"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET}
-    },
-    grenade = {
-        [ACT_MP_STAND_IDLE] = {"idle", "idle"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET}
-    },
-    melee = {
-        [ACT_MP_STAND_IDLE] = {"idle", "idle"},
-        [ACT_MP_CROUCH_IDLE] = {"idle", "idle"},
-        [ACT_MP_WALK] = {"walk", "walk"},
-        [ACT_MP_CROUCHWALK] = {"walk", "walk"},
-        [ACT_MP_RUN] = {"run", "run"},
-        [ACT_LAND] = {ACT_RESET, ACT_RESET},
-        attack = "attack1"
-    },
-    glide = ACT_IDLE,
-}
-
-/*
-[ACT_MP_STAND_IDLE] = {ACT_IDLE, ACT_IDLE},
-[ACT_MP_CROUCH_IDLE] = {ACT_IDLE, ACT_IDLE},
-[ACT_MP_WALK] = {ACT_WALK, ACT_WALK},
-[ACT_MP_CROUCHWALK] = {ACT_WALK, ACT_WALK},
-[ACT_MP_RUN] = {ACT_RUN, ACT_RUN},
-[ACT_LAND] = {ACT_RESET, ACT_RESET}
