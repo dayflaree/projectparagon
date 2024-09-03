192021ae73743c37ed48ec7652f143d1551f1042
90 <145824934+90degreeses@users.noreply.github.com>
2024-09-03 14:54:54 -0600

    Update sh_character.lua

--- schema/meta/sh_character.lua
+++ schema/meta/sh_character.lua
@@ -1,45 +1,45 @@
 local CHAR = ix.meta.character
 
 function CHAR:IsClassD()
-	return self:Team() == FACTION_CLASSD
+	return self:GetFaction() == FACTION_CLASSD
 end
 
 function CHAR:IsMaintenance()
-	return self:Team() == FACTION_MAINTENANCE
+	return self:GetFaction() == FACTION_MAINTENANCE
 end
 
 function CHAR:IsMedical()
-	return self:Team() == FACTION_MEDICAL
+	return self:GetFaction() == FACTION_MEDICAL
 end
 
 function CHAR:IsScientific()
-	return self:Team() == FACTION_SCIENTIFIC
+	return self:GetFaction() == FACTION_SCIENTIFIC
 end
 
 function CHAR:IsSecurity()
-	return self:Team() == FACTION_SECURITY
+	return self:GetFaction() == FACTION_SECURITY
 end
 
 function CHAR:IsMTF()
-	return self:Team() == FACTION_MTF
+	return self:GetFaction() == FACTION_MTF
 end
 
 function CHAR:IsSiteDirector()
-	return self:Team() == FACTION_SITEDIRECTOR
+	return self:GetFaction() == FACTION_SITEDIRECTOR
 end
 
 function CHAR:IsEthics()
-	return self:Team() == FACTION_ETHICS
+	return self:GetFaction() == FACTION_ETHICS
 end
 
 function CHAR:IsO5()
-	return self:Team() == FACTION_O5
+	return self:GetFaction() == FACTION_O5
 end
 
 function CHAR:IsSCP()
-	return self:Team() == FACTION_SCP
+	return self:GetFaction() == FACTION_SCP
 end
 
 function CHAR:IsCI()
-	return self:Team() == FACTION_CI
+	return self:GetFaction() == FACTION_CI
 end
