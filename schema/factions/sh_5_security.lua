3cdd8e60d5e19925e4f71380dfdfd284493a9542
90 <145824934+90degreeses@users.noreply.github.com>
2024-09-03 14:41:48 -0600

    Update sh_5_security.lua

--- schema/factions/sh_5_security.lua
+++ schema/factions/sh_5_security.lua
@@ -8,9 +8,4 @@ FACTION.models = {
     "models/cpthazama/scp/guard.mdl",
 }
 
-function FACTION:OnCharacterCreated(ply, char)
-    char:SetClass(CLASS_SS_CADET)
-    char:SetName("Cadet "..char:GetName())
-end
-
 FACTION_SECURITYRESPONSE = FACTION.index
