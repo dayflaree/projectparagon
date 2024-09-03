51c6a5ab002b8570de6be58ea884ef7b6a588ecf
90 <145824934+90degreeses@users.noreply.github.com>
2024-09-03 14:42:29 -0600

    Update sh_11_ci.lua

--- schema/factions/sh_11_ci.lua
+++ schema/factions/sh_11_ci.lua
@@ -8,19 +8,4 @@ FACTION.models = {
     "models/cpthazama/scp/chaos.mdl",
 }
 
-function FACTION:OnCharacterCreated(ply, char)
-    char:SetClass(CLASS_CI_CONSCRIPT)
-    char:SetName("Conscript "..char:GetName())
-end
-
-function FACTION:OnSpawn(ply)
-    local char = ply:GetCharacter()
-    char:SetClass(CLASS_CI_CONSCRIPT)
-    ply:SetArmor(250)
-end
-
-function FACTION:OnTransferred(char)
-    char:SetClass(CLASS_CI_CONSCRIPT)
-end
-
 FACTION_CI = FACTION.index
