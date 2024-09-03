cc7544350cc11ca6c32e0f14ddd6fd0cf069c4ad
90 <145824934+90degreeses@users.noreply.github.com>
2024-09-03 14:52:34 -0600

    Update sh_character.lua

--- schema/meta/sh_character.lua
+++ schema/meta/sh_character.lua
@@ -1,1 +1,45 @@
 local CHAR = ix.meta.character
+
+function CHAR:IsClassD()
+	return self:Team() == FACTION_CLASSD
+end
+
+function CHAR:IsMaintenance()
+	return self:Team() == FACTION_MAINTENANCE
+end
+
+function CHAR:IsMedical()
+	return self:Team() == FACTION_MEDICAL
+end
+
+function CHAR:IsScientific()
+	return self:Team() == FACTION_SCIENTIFIC
+end
+
+function CHAR:IsSecurity()
+	return self:Team() == FACTION_SECURITY
+end
+
+function CHAR:IsMTF()
+	return self:Team() == FACTION_MTF
+end
+
+function CHAR:IsSiteDirector()
+	return self:Team() == FACTION_SITEDIRECTOR
+end
+
+function CHAR:IsEthics()
+	return self:Team() == FACTION_ETHICS
+end
+
+function CHAR:IsO5()
+	return self:Team() == FACTION_O5
+end
+
+function CHAR:IsSCP()
+	return self:Team() == FACTION_SCP
+end
+
+function CHAR:IsCI()
+	return self:Team() == FACTION_CI
+end
