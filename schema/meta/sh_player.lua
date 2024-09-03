c51c37d56c11c61c6a28a37e9df655477a323030
90 <145824934+90degreeses@users.noreply.github.com>
2024-09-03 14:45:10 -0600

    Update sh_player.lua

--- schema/meta/sh_player.lua
+++ schema/meta/sh_player.lua
@@ -1,6 +1,6 @@
 local PLAYER = FindMetaTable("Player")
 
 -- move it if u must. couldnt find anywhere else i could put it.
-function PLAYER:IsE11()
-	return self:Team() == FACTION_E11
+function PLAYER:IsMTF()
+	return self:Team() == FACTION_MTF
 end
