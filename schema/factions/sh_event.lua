7af2e5e2f9aaeb3575879f3c621756cb42a2b47b
90 <145824934+90degreeses@users.noreply.github.com>
2024-09-03 14:44:10 -0600

    Update sh_event.lua

--- schema/factions/sh_event.lua
+++ schema/factions/sh_event.lua
@@ -4,13 +4,4 @@ FACTION.color = Color(20, 100, 140)
 
 FACTION.isDefault = false
 
-function FACTION:OnSpawn(ply)
-    local char = ply:GetCharacter()
-    char:SetClass(nil)
-end
-
-function FACTION:OnTransferred(char)
-    char:SetClass(nil)
-end
-
 FACTION_EVENT = FACTION.index
