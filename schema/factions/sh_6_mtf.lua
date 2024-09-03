5a0528d1f047c33acee52904707afa9503c3e0de
90 <145824934+90degreeses@users.noreply.github.com>
2024-09-03 14:41:33 -0600

    Update sh_6_mtf.lua

--- schema/factions/sh_6_mtf.lua
+++ schema/factions/sh_6_mtf.lua
@@ -1,4 +1,4 @@
-FACTION.name = "Mobile Task Force - Epsilon-11"
+FACTION.name = "Mobile Task Force"
 FACTION.description = ""
 FACTION.color = Color(110, 100, 200)
 
@@ -7,16 +7,5 @@ FACTION.isDefault = false
 FACTION.models = {
     "models/cpthazama/scp/ntf.mdl",
 }
-
-function FACTION:OnCharacterCreated(ply, char)
-    char:SetClass(CLASS_E11_RECRUIT)
-    char:SetName("Private "..char:GetName())
-end
-
-function FACTION:OnSpawn(ply)
-    local char = ply:GetCharacter()
-    char:SetClass(CLASS_E11_RECRUIT)
-    ply:SetArmor(250)
-end
 
 FACTION_MTF = FACTION.index
