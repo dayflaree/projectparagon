ec7969cb84f7f3b43cd3ec6908951596e4b265ce
90 <elijah.j.staley@gmail.com>
2024-09-03 21:02:34 -0600

    scp faction stuff

--- schema/factions/sh_10_scp.lua
+++ schema/factions/sh_10_scp.lua
@@ -2,6 +2,10 @@ FACTION.name = "SCP"
 FACTION.description = ""
 FACTION.color = Color(200, 50, 50)
 
-FACTION.isDefault = false
+FACTION.isDefault = true
+
+function FACTION:OnCharacterCreated(ply, char)
+    char:SetName("SCP-131-"..Schema:ZeroNumber(math.random(1, 99), 2))
+end
 
 FACTION_SCP = FACTION.index
