ecec34bee228de6b6cc125296e1bfb99bf37340d
90 <145824934+90degreeses@users.noreply.github.com>
2024-09-17 14:34:29 -0600

    Update and rename sh_3_3_scientific_researcher.lua to sh_3_3_scientific_senior.lua

--- schema/ranks/sh_3_3_scientific_researcher.lua
+++
@@ -1,4 +0,0 @@
-RANK.name = "Scientific Department - Researcher"
-RANK.faction = FACTION_SCIENTIFIC
-RANK.isDefault = false
-RANK_SCIENTIFIC_RESEARCHER = RANK.index
---
+++ schema/ranks/sh_3_3_scientific_senior.lua
@@ -0,0 +1,4 @@
+RANK.name = "Senior Researcher"
+RANK.faction = FACTION_SCIENTIFIC
+RANK.isDefault = false
+RANK_SCIENTIFIC_SENIOR = RANK.index
