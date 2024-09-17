02e13ff1df996134f8e46162f69504f419510f3b
90 <145824934+90degreeses@users.noreply.github.com>
2024-09-17 14:35:18 -0600

    Update and rename sh_3_6_scientific_specialist.lua to sh_3_4_scientific_specialist.lua

--- schema/ranks/sh_3_4_scientific_specialist.lua
+++ schema/ranks/sh_3_4_scientific_specialist.lua
@@ -1,4 +1,4 @@
-RANK.name = "Scientific Department - Containment Specialist"
+RANK.name = "Containment Specialist"
 RANK.faction = FACTION_SCIENTIFIC
 RANK.isDefault = false
 RANK_SCIENTIFIC_SPECIALIST = RANK.index
