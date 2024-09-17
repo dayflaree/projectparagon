babebd05b88b669445bb936fb9d2ab2e17ef5a75
90 <145824934+90degreeses@users.noreply.github.com>
2024-09-17 14:30:10 -0600

    Update and rename sh_2_5_maintenance_supervisor.lua to sh_2_5_maintenance_headcustodian.lua

---
+++ schema/ranks/sh_2_5_maintenance_headcustodian.lua
@@ -0,0 +1,4 @@
+RANK.name = "Head Custodian"
+RANK.faction = FACTION_MAINTENANCE
+RANK.isDefault = false
+RANK_MAINTENANCE_HEADCUSTODIAN = RANK.index
--- schema/ranks/sh_2_5_maintenance_supervisor.lua
+++
@@ -1,4 +0,0 @@
-RANK.name = "Engineering & Technical Service Department - Supervisor"
-RANK.faction = FACTION_MAINTENANCE
-RANK.isDefault = false
-RANK_MAINTENANCE_SUPERVISOR = RANK.index
