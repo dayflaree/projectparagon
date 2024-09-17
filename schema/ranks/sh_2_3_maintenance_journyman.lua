8eb8c80052046770fc0e4f28ebe64aa8936f18b5
90 <145824934+90degreeses@users.noreply.github.com>
2024-09-17 14:28:54 -0600

    Update sh_2_3_maintenance_journyman.lua

--- schema/ranks/sh_2_3_maintenance_journyman.lua
+++ schema/ranks/sh_2_3_maintenance_journyman.lua
@@ -1,4 +1,4 @@
-RANK.name = "Engineering & Technical Service Department - Journyman"
+RANK.name = "Engineer"
 RANK.faction = FACTION_MAINTENANCE
 RANK.isDefault = false
-RANK_MAINTENANCE_JOURNYMAN = RANK.index
+RANK_MAINTENANCE_ENGINEER = RANK.index
