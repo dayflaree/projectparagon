1789982c6a88e195bbfca144762db8a50c436d49
90 <145824934+90degreeses@users.noreply.github.com>
2024-09-17 14:39:20 -0600

    Update sh_4_5_security_senior.lua

--- schema/ranks/sh_4_5_security_senior.lua
+++ schema/ranks/sh_4_5_security_senior.lua
@@ -1,4 +1,4 @@
-RANK.name = "Security Department - Senior Security Officer"
+RANK.name = "Senior Security Officer"
 RANK.faction = FACTION_SECURITY
 RANK.isDefault = false
 RANK_SECURITY_SENIOR = RANK.index
