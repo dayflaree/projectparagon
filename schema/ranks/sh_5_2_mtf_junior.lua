62e4eb231dcc947f412653862be0c79387ced1a4
90 <145824934+90degreeses@users.noreply.github.com>
2024-09-17 14:44:40 -0600

    Update and rename sh_5_2_mtf_junior.lua to sh_5_2_mtf_specialist.lua

--- schema/ranks/sh_5_2_mtf_junior.lua
+++
@@ -1,4 +0,0 @@
-RANK.name = "Mobile Task Force - Junior Agent"
-RANK.faction = FACTION_MTF
-RANK.isDefault = false
-RANK_MTF_JUNIOR = RANK.index
---
+++ schema/ranks/sh_5_2_mtf_specialist.lua
@@ -0,0 +1,4 @@
+RANK.name = "Operations Specialist"
+RANK.faction = FACTION_MTF
+RANK.isDefault = false
+RANK_MTF_SPECIALIST = RANK.index
