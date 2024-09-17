99468c21fabc672620a5fedb5c0b16c74f384ab4
90 <145824934+90degreeses@users.noreply.github.com>
2024-09-17 14:38:55 -0600

    Update sh_4_1_security_junior.lua

--- schema/ranks/sh_4_1_security_junior.lua
+++ schema/ranks/sh_4_1_security_junior.lua
@@ -1,4 +1,4 @@
-RANK.name = "Security Department - Junior Security Officer"
+RANK.name = "Junior Security Officer"
 RANK.faction = FACTION_SECURITY
 RANK.isDefault = true
 RANK_SECURITY_JUNIOR = RANK.index
