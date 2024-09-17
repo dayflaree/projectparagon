877c2b9385d0645b7983a027420c9d3bf2936bb5
90 <145824934+90degreeses@users.noreply.github.com>
2024-09-17 14:29:42 -0600

    Update and rename sh_2_4_maintenance_coordinator.lua to sh_2_4_maintenance_custodian.lua

--- schema/ranks/sh_2_4_maintenance_coordinator.lua
+++
@@ -1,4 +0,0 @@
-RANK.name = "Engineering & Technical Service Department - Coordinator"
-RANK.faction = FACTION_MAINTENANCE
-RANK.isDefault = false
-RANK_MAINTENANCE_COORDINATOR = RANK.index
---
+++ schema/ranks/sh_2_4_maintenance_custodian.lua
@@ -0,0 +1,4 @@
+RANK.name = "Custodian"
+RANK.faction = FACTION_MAINTENANCE
+RANK.isDefault = false
+RANK_MAINTENANCE_CUSTODIAN = RANK.index
