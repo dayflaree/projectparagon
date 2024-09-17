6e2cc43a0ad1f703e40737777c5a800df1f2a7db
90 <145824934+90degreeses@users.noreply.github.com>
2024-09-17 14:46:02 -0600

    Update and rename sh_5_5_mtf_strategist.lua to sh_5_5_mtf_overseer.lua

---
+++ schema/ranks/sh_5_5_mtf_overseer.lua
@@ -0,0 +1,4 @@
+RANK.name = "Overseer"
+RANK.faction = FACTION_MTF
+RANK.isDefault = false
+RANK_MTF_OVERSEER = RANK.index
--- schema/ranks/sh_5_5_mtf_strategist.lua
+++
@@ -1,4 +0,0 @@
-RANK.name = "Mobile Task Force - Strategist"
-RANK.faction = FACTION_MTF
-RANK.isDefault = false
-RANK_MTF_STRATEGIST = RANK.index
