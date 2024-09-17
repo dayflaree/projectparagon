6c97674c6b839fb2470d3719804c5a8741b40a03
90 <145824934+90degreeses@users.noreply.github.com>
2024-09-17 14:44:10 -0600

    Update and rename sh_5_1_mtf_interim.lua to sh_5_1_mtf_junior.lua

--- schema/ranks/sh_5_1_mtf_interim.lua
+++
@@ -1,4 +0,0 @@
-RANK.name = "Mobile Task Force - Interim Agent"
-RANK.faction = FACTION_MTF
-RANK.isDefault = true
-RANK_MTF_INTERIM = RANK.index
---
+++ schema/ranks/sh_5_1_mtf_junior.lua
@@ -0,0 +1,4 @@
+RANK.name = "Junior Operative"
+RANK.faction = FACTION_MTF
+RANK.isDefault = true
+RANK_MTF_JUNIOR = RANK.index
