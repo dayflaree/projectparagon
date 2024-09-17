72ece976882f5d8124d08941eb84b823fc7f0a12
90 <145824934+90degreeses@users.noreply.github.com>
2024-09-17 14:34:04 -0600

    Update and rename sh_3_2_scientific_junior.lua to sh_3_2_scientific_analyst.lua

---
+++ schema/ranks/sh_3_2_scientific_analyst.lua
@@ -0,0 +1,4 @@
+RANK.name = "Research Analyst"
+RANK.faction = FACTION_SCIENTIFIC
+RANK.isDefault = false
+RANK_SCIENTIFIC_ANALYST = RANK.index
--- schema/ranks/sh_3_2_scientific_junior.lua
+++
@@ -1,4 +0,0 @@
-RANK.name = "Scientific Department - Junior Researcher"
-RANK.faction = FACTION_SCIENTIFIC
-RANK.isDefault = false
-RANK_SCIENTIFIC_JUNIOR = RANK.index
