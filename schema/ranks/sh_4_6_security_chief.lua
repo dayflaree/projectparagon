c2c6bcde6fe874c10e8012f74693b1f970a32525
90 <145824934+90degreeses@users.noreply.github.com>
2024-09-17 14:41:43 -0600

    Update and rename sh_4_7_security_coordinator.lua to sh_4_6_security_chief.lua

---
+++ schema/ranks/sh_4_6_security_chief.lua
@@ -0,0 +1,4 @@
+RANK.name = "Security Chief"
+RANK.faction = FACTION_SECURITY
+RANK.isDefault = false
+RANK_SECURITY_CHIEF = RANK.index
--- schema/ranks/sh_4_7_security_coordinator.lua
+++
@@ -1,4 +0,0 @@
-RANK.name = "Security Department - Security Coordinator"
-RANK.faction = FACTION_SECURITY
-RANK.isDefault = false
-RANK_SECURITY_COORDINATOR = RANK.index
