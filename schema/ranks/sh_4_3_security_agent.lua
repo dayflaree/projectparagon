190b88972042c568e4a1b3d10d182c394eb11d17
90 <145824934+90degreeses@users.noreply.github.com>
2024-09-17 14:38:04 -0600

    Update and rename sh_4_3_security_associate.lua to sh_4_3_security_agent.lua

---
+++ schema/ranks/sh_4_3_security_agent.lua
@@ -0,0 +1,4 @@
+RANK.name = "Security Agent"
+RANK.faction = FACTION_SECURITY
+RANK.isDefault = false
+RANK_SECURITY_AGENT = RANK.index
--- schema/ranks/sh_4_3_security_associate.lua
+++
@@ -1,4 +0,0 @@
-RANK.name = "Security Department - Security Associate"
-RANK.faction = FACTION_SECURITY
-RANK.isDefault = false
-RANK_SECURITY_ASSOCIATE = RANK.index
