bfb9af68f291ff00e995692925397964d1c2fece
90 <145824934+90degreeses@users.noreply.github.com>
2024-09-17 14:40:39 -0600

    Update and rename sh_4_6_security_operator.lua to sh_4_5_security_sergeant.lua

---
+++ schema/ranks/sh_4_5_security_sergeant.lua
@@ -0,0 +1,4 @@
+RANK.name = "Security Sergeant"
+RANK.faction = FACTION_SECURITY
+RANK.isDefault = false
+RANK_SECURITY_SERGEANT = RANK.index
--- schema/ranks/sh_4_6_security_operator.lua
+++
@@ -1,4 +0,0 @@
-RANK.name = "Security Department - Security Operator"
-RANK.faction = FACTION_SECURITY
-RANK.isDefault = false
-RANK_SECURITY_OPERATOR = RANK.index
