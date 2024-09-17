a6522fa80b4f944ef4cb73d4a8df7dc92b28d4cb
90 <145824934+90degreeses@users.noreply.github.com>
2024-09-17 14:46:27 -0600

    Update and rename sh_5_6_mtf_coordinator.lua to sh_5_6_mtf_commander.lua

---
+++ schema/ranks/sh_5_6_mtf_commander.lua
@@ -0,0 +1,4 @@
+RANK.name = "Commander"
+RANK.faction = FACTION_MTF
+RANK.isDefault = false
+RANK_MTF_COMMANDER = RANK.index
--- schema/ranks/sh_5_6_mtf_coordinator.lua
+++
@@ -1,4 +0,0 @@
-RANK.name = "Mobile Task Force - Coordinator"
-RANK.faction = FACTION_MTF
-RANK.isDefault = false
-RANK_MTF_COORDINATOR = RANK.index
