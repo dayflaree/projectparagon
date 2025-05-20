c7737508b720ee68b3ca38536bbcaf09224c2f9f
dayflare <145824934+dayflaree@users.noreply.github.com>
2025-05-19 23:00:59 -0600

    SCP Faction WIP

--- schema/factions/sh_9_scp.lua
+++ schema/factions/sh_9_scp.lua
@@ -1,24 +1,6 @@
 FACTION.name = "Anomaly"
 FACTION.color = Color(173, 40, 40)
-FACTION.isDefault = true
+FACTION.isDefault = false
 FACTION.models = {"models/cpthazama/scp/173.mdl"}
-
-function FACTION:OnSpawn(client)
-    local char = client:GetCharacter()
-    if not char then return end
-
-    local steamID = client:SteamID()
-
-    -- SteamID-to-Class map
-    local classAssignments = {
-        ["STEAM_0:1:80008500"] = CLASS_SCP173,
-    }
-
-    local assignedClass = classAssignments[steamID]
-
-    if assignedClass then
-        char:SetClass(assignedClass)
-    end
-end
 
 FACTION_SCP = FACTION.index
